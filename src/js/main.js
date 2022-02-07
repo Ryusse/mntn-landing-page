13d921ab59d7b4d39bf1a893b275b5908b7f3e0d
Joel <joelangeltw@gmail.com>
2022-02-07 09:02:46 -0500

    header responsuve

--- src/js/main.js
+++ src/js/main.js
@@ -1,3 +1,13 @@
-import "../css/base.css";
-import "../css/style.css";
-import "../css/normalize.css";
+import '../css/base.css'
+import '../css/style.css'
+import '../css/normalize.css'
+
+const header = document.querySelector('header')
+
+window.onscroll = () => {
+  if (window.scrollY > 100) {
+    header.classList.add('scroll-active')
+  } else {
+    header.classList.remove('scroll-active')
+  }
+}
